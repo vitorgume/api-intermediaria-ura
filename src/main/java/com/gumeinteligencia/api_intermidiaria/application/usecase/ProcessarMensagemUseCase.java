0542a16a02c3b88046ea2f55b666afd2518785e3
vitorgume <vitorvieira@gumeinteligencia.com.br>
2025-08-20 14:36:18 -0300

    Ajustando código para teste do chatbot

--- src/main/java/com/gumeinteligencia/api_intermidiaria/application/usecase/ProcessarMensagemUseCase.java
+++ src/main/java/com/gumeinteligencia/api_intermidiaria/application/usecase/ProcessarMensagemUseCase.java
@@ -32,22 +32,24 @@ public class ProcessarMensagemUseCase {
                 .ifPresentOrElse(contexto -> {
                     contextoUseCase.processarContextoExistente(contexto, mensagem);
                 }, () -> {
-                    Optional<Cliente> cliente = clienteUseCase.consultarPorTelefone(mensagem.getTelefone());
+//                    Optional<Cliente> cliente = clienteUseCase.consultarPorTelefone(mensagem.getTelefone());
+//
+//                    if(cliente.isEmpty()) {
+//                        boolean usarChatbot = roteadorDeTrafegoUseCase.deveUsarChatbot(mensagem.getTelefone());
+//                        if (usarChatbot) {
+//                            contextoUseCase.iniciarNovoContexto(mensagem);
+//                        } else {
+//                            uraUseCase.enviar(mensagem);
+//                        }
+//                    } else {
+//                        if (cliente.get().getCanal().getCodigo() == 0) {
+//                            contextoUseCase.iniciarNovoContexto(mensagem);
+//                        } else {
+//                            uraUseCase.enviar(mensagem);
+//                        }
+//                    }
 
-                    if(cliente.isEmpty()) {
-                        boolean usarChatbot = roteadorDeTrafegoUseCase.deveUsarChatbot(mensagem.getTelefone());
-                        if (usarChatbot) {
-                            contextoUseCase.iniciarNovoContexto(mensagem);
-                        } else {
-                            uraUseCase.enviar(mensagem);
-                        }
-                    } else {
-                        if (cliente.get().getCanal().getCodigo() == 0) {
-                            contextoUseCase.iniciarNovoContexto(mensagem);
-                        } else {
-                            uraUseCase.enviar(mensagem);
-                        }
-                    }
+                    contextoUseCase.iniciarNovoContexto(mensagem);
 
                 });
 
