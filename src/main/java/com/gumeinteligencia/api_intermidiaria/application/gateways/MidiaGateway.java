e327819e8e97c5daed1b05d21c417c88e04b4c6d
vitorgume <vitorhvvieira@gmail.com>
2025-11-03 16:18:41 -0300

    Retirando por completo feature de midia do usuário

--- src/main/java/com/gumeinteligencia/api_intermidiaria/application/gateways/MidiaGateway.java
+++
@@ -1,7 +0,0 @@
-package com.gumeinteligencia.api_intermidiaria.application.gateways;
-
-import com.gumeinteligencia.api_intermidiaria.domain.MidiaCliente;
-
-public interface MidiaGateway {
-    void salvar(MidiaCliente midiaCliente);
-}
--- src/main/java/com/gumeinteligencia/api_intermidiaria/application/usecase/MidiaUseCase.java
+++ src/main/java/com/gumeinteligencia/api_intermidiaria/application/usecase/MidiaUseCase.java
@@ -1,42 +1,18 @@
 package com.gumeinteligencia.api_intermidiaria.application.usecase;
 
-import com.gumeinteligencia.api_intermidiaria.application.gateways.MidiaGateway;
 import com.gumeinteligencia.api_intermidiaria.domain.Mensagem;
-import com.gumeinteligencia.api_intermidiaria.domain.MidiaCliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MidiaUseCase {
 
-    private final MidiaGateway gateway;
-
     public Mensagem extrairMidias(Mensagem mensagem) {
 
-        MidiaCliente midiaCliente = MidiaCliente.builder().telefoneCliente(mensagem.getTelefone()).urlMidias(new ArrayList<>()).build();
-
-        if(!mensagem.getUrlAudio().isBlank()) {
-            midiaCliente.adicionarUrl(mensagem.getUrlAudio());
-            gateway.salvar(midiaCliente);
-            mensagem.setMensagem("Midia do usuário");
-            return mensagem;
-        }
-
-        if (!mensagem.getUrlImagem().isBlank()) {
-            midiaCliente.adicionarUrl(mensagem.getUrlImagem());
-            gateway.salvar(midiaCliente);
-            mensagem.setMensagem("Midia do usuário");
-            return mensagem;
-        }
-
-        if(!mensagem.getUrlVideo().isBlank()) {
-            midiaCliente.adicionarUrl(mensagem.getUrlVideo());
-            gateway.salvar(midiaCliente);
+        if(!mensagem.getUrlAudio().isBlank() || !mensagem.getUrlImagem().isBlank() || !mensagem.getUrlVideo().isBlank()) {
             mensagem.setMensagem("Midia do usuário");
             return mensagem;
         }
